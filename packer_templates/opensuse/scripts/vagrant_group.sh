39dffa03e3bc95c6fdc825f5490a99189899fd89
Ali Rizvi-Santiago <arizvisa@gmail.com>
2017-12-15 11:25:32 -0500

    Changed all instances of the regular user (`vagrant:vagrant`) auth sequence to `user:user` and administrative user (`root:vagrant`) to `root:default user password`. Documented the new authentication in README.md.

--- packer_templates/opensuse/scripts/vagrant_group.sh
+++ packer_templates/opensuse/scripts/vagrant_group.sh
@@ -3,5 +3,5 @@
 # User 'vagrant' belogs to the 'users' group by default so we need to
 # create a new group 'vagrant' and put our user there.
 
-groupadd -f vagrant
-gpasswd -a vagrant vagrant
+groupadd -f users
+gpasswd -a user user
